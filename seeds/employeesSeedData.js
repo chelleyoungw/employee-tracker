e7dd6edf87d7693c981c0369e4c31ec57e600abe
Chelle Wood <chelleyoungw@gmail.com>
2024-04-30 21:34:16 -0500

    added data to seed employee table

---
+++ seeds/employeesSeedData.js
@@ -0,0 +1,50 @@
+[
+  {
+    first_name: "Damien",
+    last_name: "Ivory",
+    role_id: 1,
+    manager_id: null,
+  },
+  {
+    first_name: "Chelle",
+    last_name: "Wood",
+    role_id: 2,
+    manager_id: 1,
+  },
+  {
+    first_name: "Layla",
+    last_name: "Smith",
+    role_id: 3,
+    manager_id: 2,
+  },
+  {
+    first_name: "Tom",
+    last_name: "Sanderson",
+    role_id: 4,
+    manager_id: 1,
+  },
+  {
+    first_name: "Marie",
+    last_name: "Xenley",
+    role_id: 5,
+    manager_id: 2,
+  },
+  {
+    first_name: "Constantine",
+    last_name: "Brown",
+    role_id: 6,
+    manager_id: 4,
+  },
+  {
+    first_name: "Lily",
+    last_name: "Ork",
+    role_id: 7,
+    manager_id: 4,
+  },
+  {
+    first_name: "Jonny",
+    last_name: "Deep",
+    role_id: 8,
+    manager_id: 2,
+  },
+];
